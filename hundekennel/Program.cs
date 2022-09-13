7d6d2e2080637e3b25d3ea6b81c83ffefd4dea78
RajaaRumy <raru42946@edu.ucl.dk>
2022-09-13 13:59:08 +0200

    Add project files.

---
+++ hundekennel/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+namespace hundekennel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog[] dog = new Dog[1000];
+            
+            for (int i = 0; i < dog.Length; i++) {
+                
+                Console.WriteLine("How many dogs do you went to add, and Add a dog´s information");
+                Console.ReadLine();
+                Console.WriteLine("Enter a dog´s ID");
+                dog[i].id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a dog´s PedigreeNumber");
+                dog[i].pedigreeNumber = Console.ReadLine();
+                Console.WriteLine("Enter a dog´s name");
+                dog[i].name = Console.ReadLine();
+                Console.WriteLine(" Enter a dogs Birthday: Day-month- year");
+                dog[i].birthday = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" Enter a dogs Koen, H for Male and T for female");
+                 dog[i].koen = Console.ReadLine();
+                Console.WriteLine("Enter a dogs Color");
+                dog[i].color = Console.ReadLine();
+                Console.WriteLine("Enter a dogs chipNumber");
+                 dog[i].chipNumber = Console.ReadLine();
+                Console.WriteLine("Enter a dog´s FatherssPedigeeNumber");
+                dog[i].fatherssPedigeeNumber = Console.ReadLine();
+                Console.WriteLine("Enter a dogs MotherssPedigeeNumber");
+                dog[i].motherssPedigeeNumber = Console.ReadLine();
+
+                for (int j = 0; j< dog.Length; j++) {
+                    
+                    Console.WriteLine($" Dog´s ID is :{0}\n Dog´s PedigreeNumber is {1}\n Dog´s Name is: {2} \n Dogs Birthday is : {3} \n Dogs koen is  : {4} \n Dogs Color is:{5} \n Dogs chipNumber is: {6}\n Dogs FatherssPedigeeNumber is {7}\n Dogs MotherssPedigeeNumber is {8} ", dog[i].id, dog[i].PedigreeNumber, dog[i].name, dog[i].birthday, dog[i].koen, dog[i].color, dog[i].chipNumber, dog[i].fatherssPedigeeNumber, dog[i].motherssPedigeeNumber);
+
+
+                }
+                //// skrive til file
+                StreamWriter writer = new StreamWriter("filskrive1.txt", true);
+                writer.WriteLine($" Dog´s ID is :{0}\n Dog´s PedigreeNumber is {1}\n Dog´s Name is: {2} \n Dogs Birthday is : {3} \n Dogs koen is  : {4} \n Dogs Color is:{5} \n Dogs chipNumber is: {6}\n Dogs FatherssPedigeeNumber is {7}\n Dogs MotherssPedigeeNumber is {8} ", dog[i].id, dog[i].PedigreeNumber, dog[i].name, dog[i].birthday, dog[i].koen, dog[i].color, dog[i].chipNumber, dog[i].fatherssPedigeeNumber, dog.motherssPedigeeNumber);
+
+
+                writer.Close(); // for at lukke
+                Console.ReadKey();
+
+
+            }
+
+            //static void ReadFile(string path)
+            //{// brug loop (for )
+            //    StreamReader reader = new StreamReader(path);
+            //    string FileData = reader.ReadToEnd();
+            //    Console.WriteLine(FileData);
+            //}
+        }
+
+    }
+}
---
+++ hundekennel/dogs.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hundekennel
+{
+    public class Dog
+    {
+        public int id;
+        public string pedigreeNumber;
+        public string name;
+        public int birthday;
+        public string koen;
+        public string color;
+        public string chipNumber;
+        public string fatherssPedigeeNumber;
+        public string motherssPedigeeNumber;
+
+        public Dog(int id, string pedigreeNumber, string name, int birthday,string koen, string color, string chipNumber, string fatherssPedigeeNumber, string motherssPedigeeNumber)
+        {
+            this.id = id;
+            this.pedigreeNumber = pedigreeNumber;   
+            this.name = name;
+            this.birthday = birthday;   
+            this.koen = koen;
+            this.color = color;
+            this.chipNumber = chipNumber;
+            this.fatherssPedigeeNumber = fatherssPedigeeNumber;
+            this.motherssPedigeeNumber=motherssPedigeeNumber;
+
+        }
+
+        public int ID  
+        {
+            get { return id; } 
+            set { id = value; } 
+        }
+        public string PedigreeNumber
+        {
+             get { return pedigreeNumber; }
+             set { pedigreeNumber = value;}
+        }
+        public string Name
+        {
+            get { return name; }   
+            set { name = value; }
+        }
+
+        public int BirthdayDate
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+        public string Koen
+        {
+            get { return koen; }
+            set { koen = value; }
+        }
+        public string Color
+        {
+            get { return color; }   
+            set { color = value; }  
+        }
+        public string ChipNumber
+        {
+            get { return chipNumber; }
+            set { chipNumber = value; }
+        }
+        public string FatherssPedigeeNumber
+        {
+            get { return fatherssPedigeeNumber; }   
+            set { fatherssPedigeeNumber = value; }
+        }
+        public string MotherssPedigeeNumber
+        {
+            get { return motherssPedigeeNumber; }
+            set { motherssPedigeeNumber = value; }
+        }
+
+        public Dog()
+        {
+
+
+        }
+        public void print()
+        {
+            //Console.WriteLine($" Dog´s ID is :{0}\n Dog´s PedigreeNumber is {1}\n Dog´s Name is: {2} \n Dogs Birthday is : {3} \n Dogs koen is  : {4} \n Dogs Color is:{5} \n Dogs chipNumber is: {6}\n Dogs FatherssPedigeeNumber is {7}\n Dogs MotherssPedigeeNumber is {8} ", id, PedigreeNumber, name, birthday, koen, color, chipNumber ,fatherssPedigeeNumber,motherssPedigeeNumber);
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
